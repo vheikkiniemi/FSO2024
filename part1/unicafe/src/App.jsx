109b181e9586c83c338b9d3f550e103cf14874fb
Ville Heikkiniemi <ville.heikkiniemi@gmail.com>
2024-03-02 21:53:05 +0200

    part1-11 - unicafe step6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,7 +6,13 @@ const Button = (props) => (
   </button>
 )
 
-const StatisticLine = ({ text, value }) => <div>{text} {value}</div>
+const StatisticLine = ({ text, value }) => {
+  return (
+    <>
+      <tr><td>{text}</td><td>{value}</td></tr>
+    </>
+  )
+}
 
 const Statistics = ({good, neutral, bad}) => {
   if (good === 0 && neutral === 0 && bad === 0) {
@@ -19,12 +25,16 @@ const Statistics = ({good, neutral, bad}) => {
 
   return (
     <div>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={good+neutral+bad} />
-      <StatisticLine text="average" value={(good*1+neutral*0+bad*(-1))/(good+neutral+bad)} />
-      <StatisticLine text="positive" value={String((good)/(good+neutral+bad)*100) + " %"} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={good+neutral+bad} />
+          <StatisticLine text="average" value={(good*1+neutral*0+bad*(-1))/(good+neutral+bad)} />
+          <StatisticLine text="positive" value={String((good)/(good+neutral+bad)*100) + " %"} />
+        </tbody>
+      </table>
     </div>
   )
 }
